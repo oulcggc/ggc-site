5a8198bc2760f72d0664764aa3af2cb612ef6400
yocjyet <git@yocjyet.dev>
2023-11-07 00:17:27 +0900

    Add LINE OpenChat QRCode

--- src/lib/sections/SectionContact.svelte
+++ src/lib/sections/SectionContact.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	import SNSIcon from '$lib/ui/SNSIcon.svelte';
+	import QRCode from '$lib/ui/QRCode.svelte';
 
 	import SNS_ACCOUNTS from '$data/sns.json';
+	import OPENCHAT_LINK from '$data/openchat-link.txt?raw';
+	import OPENCHAT_LOGO from '$assets/line_openchat_400x400.png';
 </script>
 
 <h2>お問い合わせ</h2>
@@ -13,4 +16,39 @@
 			{name}
 		</a>
 	{/each}
+	<div class="openchat-qr">
+		<div class="qr-logo">
+			<img src={OPENCHAT_LOGO} alt="LINEオープンチャット" title="LINEオープンチャット" />
+		</div>
+		<QRCode text={OPENCHAT_LINK} />
+	</div>
 </div>
+
+<style>
+	.openchat-qr {
+		position: relative;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		width: fit-content;
+	}
+
+	.openchat-qr .qr-logo {
+		position: absolute;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.openchat-qr img {
+		width: 20%;
+		height: 20%;
+		border-radius: 50%;
+		border: 3px solid white;
+		box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.5);
+	}
+</style>
---
+++ src/lib/ui/QRCode.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import QRCode from 'qrcode';
+
+	export let text: string;
+</script>
+
+{#await QRCode.toDataURL(text, { errorCorrectionLevel: 'H' }) then src}
+	<img {src} alt={text} title={text} />
+{/await}
