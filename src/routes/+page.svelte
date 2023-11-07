fc3715b0d98589ea457f259d6a894fcf6895864b
yocjyet <git@yocjyet.dev>
2023-11-07 10:09:47 +0900

    Add nav link hover effect

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -90,4 +90,31 @@
 	ul.sns {
 		font-size: 2.5em;
 	}
+
+	nav a {
+		color: inherit;
+		text-decoration: none;
+		transition: color 0.2s ease-in-out;
+		position: relative;
+	}
+
+	nav a:hover {
+		/* text-decoration: underline; */
+		color: #184164;
+	}
+
+	nav a::after {
+		content: '';
+		display: block;
+		position: absolute;
+		width: 0;
+		height: 2px;
+		bottom: -0.2em;
+		background: #184164;
+		transition: width 0.3s ease-in-out;
+	}
+
+	nav a:hover::after {
+		width: 100%;
+	}
 </style>
