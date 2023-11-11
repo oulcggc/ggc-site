34bca2ae83eaaada10238fe0bf20611c588b5b97
yocjyet <git@yocjyet.dev>
2023-11-11 21:50:06 +0900

    Add blog navigation

--- src/routes/blog/+layout.svelte
+++ src/routes/blog/+layout.svelte
@@ -1,22 +1,60 @@
 <script>
 	import NavMenu from '$lib/ui/NavMenu.svelte';
+	import GGCLogo from '$assets/ggc_logo_1240x1240.jpg';
+	import { page } from '$app/stores';
+	$: console.log($page);
 </script>
 
-<div>
-	<NavMenu>
-		<li>
-			<a href="/"> ホーム </a>
-		</li>
-		<li>
-			<a href="/blog"> ブログ </a>
-		</li>
-	</NavMenu>
-</div>
+<header>
+	<a class="logo" href="/">
+		<img src={GGCLogo} alt="GGCロゴ" />
+		<span> 阪大言語サークルGGC </span>
+	</a>
+
+	<nav>
+		<a class="text" href="/"> ホーム </a>
+		<a class="text" href="/blog"> ブログ </a>
+		<a class="text" href="/#contact">お問い合わせ</a>
+	</nav>
+</header>
 
 <slot />
 
 <style>
-	div {
-		padding: 1.5em;
+	header {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+
+		border-bottom: 1px solid #eaeaea;
+
+		padding: 0.5em 1em;
+
+		margin-bottom: 2em;
+	}
+
+	a.logo {
+		display: flex;
+		align-items: center;
+		gap: 0.5em;
+		text-decoration: none;
+	}
+
+	header img {
+		width: 3em;
+		height: 3em;
+		margin-right: 0.5em;
+	}
+
+	header span {
+		font-size: 1.25em;
+		font-weight: bold;
+		color: #184164;
+	}
+
+	nav {
+		display: flex;
+		align-items: center;
+		gap: 0.75em;
 	}
 </style>
