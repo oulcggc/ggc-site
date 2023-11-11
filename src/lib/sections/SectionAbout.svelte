a20ab0be2cc33f1b9d20d70d1a3eb876afdcfc62
yocjyet <git@yocjyet.dev>
2023-11-12 02:15:14 +0900

    Adjust SectionAbout appearance

--- src/lib/sections/SectionAbout.svelte
+++ src/lib/sections/SectionAbout.svelte
@@ -1,6 +1,6 @@
-<h2>サークル紹介</h2>
+<div class="about">
+	<h2>サークル紹介</h2>
 
-<div class="text">
 	<p>
 		大阪大学の箕面キャンパスと豊中キャンパスを拠点に活動している言語サークルです。
 		名前のGGCは、語学・言語学サークル（<b>G</b>ogaku <b>G</b>engogaku
@@ -13,11 +13,38 @@
 		時には浅く広く文字・発音・語彙・文法などの基礎から一緒に一つの言語を勉強して語学を楽しんだり、時には深く掘り下げて言語学の専門知識を駆使し言語へ共時的・通時的考察したり、時には頑張って勉強してきた語学力を試す場としての言語交換を実践したりしています。
 	</p>
 	<p>一緒に、言語について学び、楽しみましょう！</p>
+	<p style="text-align: right;">
+		<a href="#" class="text" title="制作中">サークル沿革(TBD)</a>
+		<a href="#contact" class="text">お問い合わせ</a>
+	</p>
 </div>
 
 <style>
-	.text {
+	.about {
 		margin: 0 auto;
-		max-width: 40em;
+		max-width: 43em;
+
+		border: 1px solid #ccc;
+		border-radius: 0.5em;
+		padding: 2em 4em;
+
+		display: grid;
+		grid-template-columns: 1fr;
+		gap: 1em;
+	}
+
+	@media (max-width: 768px) {
+		.about {
+			padding: 2em;
+		}
+	}
+
+	h2,
+	p {
+		margin: 0;
+	}
+
+	h2 {
+		text-align: center;
 	}
 </style>
