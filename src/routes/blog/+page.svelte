2d7cf281abbd0b42c94128e37c9986214c64a9b8
yocjyet <git@yocjyet.dev>
2023-11-11 22:56:40 +0900

    Add Footer to blog list

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import BlogResult from '$lib/ui/blog/BlogResult.svelte';
+	import RiCopyrightLine from '~icons/ri/copyright-line';
+
 	import type { PageData } from './$types';
 
 	export let data: PageData;
@@ -13,6 +15,14 @@
 	{/each}
 </main>
 
+<hr />
+
+<footer>
+	<p>
+		<RiCopyrightLine /> 2022-2023 阪大言語サークルGGC
+	</p>
+</footer>
+
 <style>
 	main {
 		max-width: 800px;
@@ -23,5 +33,20 @@
 		align-items: stretch;
 		justify-content: stretch;
 		gap: 2em;
+	}
+
+	hr {
+		margin-top: 2em;
+	}
+
+	footer p {
+		margin: 0;
+
+		padding: 1em;
+
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		gap: 0.25em;
 	}
 </style>
